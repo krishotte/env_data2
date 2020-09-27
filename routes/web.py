9e9276420653bf9103cb5536aeb5efbb272e8eae
Peter Krssak <krishotte@seznam.cz>
2020-09-27 21:26:06 +0200

    updated routes, created api controller

--- routes/web.py
+++ routes/web.py
@@ -6,8 +6,15 @@ ROUTES = [
     Get('/', 'WelcomeController@show').name('welcome'),
 
     Post('/envdata', 'DataController@store2'),
-    # Post('/envdata2', 'DataController@store2'),
 
+    # Home route
+    Get('/main', 'DataController@show_main'),
+    # Data presentation routes
     Get('/envdata/@device_id/@data_length', 'DataController@show_device_data'),
     Get('/envdata-last', 'DataController@show_last'),
+    Get('/envdata-stat', 'DataController@show_stat'),
+
+    # API routes
+    Get('/api/envdata-last', 'ApiController@show_last'),
+    Get('/api/envdata/@device_id/@data_length', 'ApiController@show_device_data')
 ]
